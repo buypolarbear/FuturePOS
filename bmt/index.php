b9de2bf172ddfba06fd06dbe10b087ee29f1f95a
Sebastiano Cataudo <sebastiano@wearefuturing.com>
2018-03-28 18:41:29 +0200

    first commit

--- bmt/index.php
+++ bmt/index.php
@@ -1,4 +1,4 @@
-<?php 
+<?php
 	include('inc/initialize.php');
 	echo '<link href="https://fonts.googleapis.com/css?family=VT323" rel="stylesheet">';
 	echo '<style>body{background:#000; color:green; font-family: "VT323";}</style>';
